303302bf5d5012e2d8aaacc72d7bab9afff8d657
宋浩茹 <eerica841205@gmail.com>
2021-04-20 21:20:27 +0800

    create App Api

---
+++ FunPayAppServices/FunPayAppServices/Api/ItemApiController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FunPayAppServices.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FunPayAppServices.Controllers
+{
+    [Route("api/[controller]")]
+    public class ItemApiController : Controller
+    {
+
+        private readonly IItemRepository ItemRepository;
+
+        public ItemApiController(IItemRepository itemRepository)
+        {
+            ItemRepository = itemRepository;
+        }
+
+        [HttpGet]
+        public IActionResult List()
+        {
+            return Ok(ItemRepository.GetAll());
+        }
+
+        [HttpGet("{id}")]
+        public Item GetItem(string id)
+        {
+            Item item = ItemRepository.Get(id);
+            return item;
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody]Item item)
+        {
+            try
+            {
+                if (item == null || !ModelState.IsValid)
+                {
+                    return BadRequest("Invalid State");
+                }
+
+                ItemRepository.Add(item);
+
+            }
+            catch (Exception)
+            {
+                return BadRequest("Error while creating");
+            }
+            return Ok(item);
+        }
+
+        [HttpPut]
+        public IActionResult Edit([FromBody] Item item)
+        {
+            try
+            {
+                if (item == null || !ModelState.IsValid)
+                {
+                    return BadRequest("Invalid State");
+                }
+                ItemRepository.Update(item);
+            }
+            catch (Exception)
+            {
+                return BadRequest("Error while creating");
+            }
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(string id)
+        {
+            ItemRepository.Remove(id);
+        }
+    }
+}
